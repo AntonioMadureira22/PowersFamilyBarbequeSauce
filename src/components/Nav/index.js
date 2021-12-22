fc12964fa60f111d34409d791bb0c64ee2db4a91
Philip Powers <philip_powers607@hotmail.com>
2021-12-22 09:00:07 -0500

    basics of layout added

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import { stack as Menu } from 'react-burger-menu';
+import React from "react";
+import { stack as Menu } from "react-burger-menu";
 
-function Nav() {
-class Example extends React.Component {
-    showSettings (event) {
-        event.preventDefault();
+
+
+class Nav extends React.Component {
+    showSettings(event) {
+      event.preventDefault();
     }
-    render () {
-        return (
-            <Menu>
-                <a id="about" className="menu-item">About</a>
-                <a id="store" className="menu-item">Store</a>
-                <a id="contact" className="menu-item">Contact</a>
-                <a id="events" className="menu-item">Events</a>
-                <a onClick={ this.showSettings } className="menu-item--small" href=""></a>
-            </Menu>
-        );
+    render() {
+      return (
+        <Menu noOverlay>
+          <a href="/" id="home" className="menu-item">
+            Home✩
+          </a>
+          <br/>
+          <a href="#about" id="about" className="menu-item">
+            About✩
+          </a>
+          <br/>
+          <a href="#store" id="store" className="menu-item">
+            Store✩
+          </a>
+          <br/>
+          <a href="#contact" id="contact" className="menu-item">
+            Contact✩
+          </a>
+          <br/>
+          <a href="#events" id="events" className="menu-item">
+            Events✩
+          </a>
+          <a
+            onClick={this.showSettings}
+            className="menu-item--small"
+            href=""
+          ></a>
+        </Menu>
+      );
     }
-}
-}
+  }
+
 
 export default Nav;
