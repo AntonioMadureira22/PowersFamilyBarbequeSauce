b33058cbdeae4f3f7be92ab2a77622b726da77be
Philip Powers <philip_powers607@hotmail.com>
2021-12-23 19:14:28 -0500

    commented out carousel

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import { Carousel } from 'react-responsive-carousel';
+// import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+// import { Carousel } from 'react-responsive-carousel';
 
-class DemoCarousel extends Component {
-    render() {
-        return (
-            <Carousel>
-                <div>
-                    <img src='assets/BBQ-in-snow.jpg' />
-                </div>
-                <div>
-                    <img src='assets/Chicken.jpg' />
-                </div>
-            </Carousel>
-        );
-    }
-};
+// class DemoCarousel extends Component {
+//     render() {
+//         return (
+//             <Carousel>
+//                 <div>
+//                     <img src='assets/BBQ-in-snow.jpg' />
+//                 </div>
+//                 <div>
+//                     <img src='assets/Chicken.jpg' />
+//                 </div>
+//             </Carousel>
+//         );
+//     }
+// };
 
-ReactDOM.render(<DemoCarousel/>, document.querySelector('.demo-carousel'));
+// ReactDOM.render(<DemoCarousel/>, document.querySelector('.demo-carousel'));
 
-export default DemoCarousel;
+// export default DemoCarousel;
