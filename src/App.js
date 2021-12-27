e1b68433304ef6c51d7f4053cae17fbf9984ecad
Xueting Hu <huxu@uwplatt.edu>
2021-12-26 20:43:27 -0500

    reverting back to the working version

---
+++ src/App.js
@@ -0,0 +1,26 @@
+// import './App.css';
+import React from 'react';
+import './Nav.css';
+import Nav from './components/Nav'
+import DemoCarousel from './components/Carousel';
+// import About from './components/About'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Nav></Nav>
+        <h2 className='Name'>
+        Powers Family ✩ Barbeque Sauce
+        </h2>
+        <br/>
+        <div className='slogan'>
+          Made By Our Family Enjoyed By Yours
+        </div>
+      </header>
+    </div>
+
+  );
+}
+
+export default App;
