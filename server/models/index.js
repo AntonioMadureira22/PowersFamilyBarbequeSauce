115765c87fb86db988f6e098f602c1d128cf05bf
Philip Powers <philip_powers607@hotmail.com>
2021-12-29 21:04:01 -0500

    get develop

--- server/models/index.js
+++
@@ -1,6 +0,0 @@
-const User = require("./User");
-const Sauce = require("./Sauce");
-const Order = require('./Order');
-const Category = require('./Category');
-
-module.exports = { User, Sauce, Order, Category };
