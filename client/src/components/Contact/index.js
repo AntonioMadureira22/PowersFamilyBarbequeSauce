e47762dbe73a1bbfb6a5f76011e2519985321cba
MarkCyg <87244359+MarkCyg@users.noreply.github.com>
2022-01-03 20:25:01 -0500

    contact info

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 function Contact() {
     return (
-        <section></section>
+        <section>
+             <h2 class="container">Contact Us</h2>
+             <br/>
+             <br/>
+             <p><a href="tel:">(860) 215-0051</a></p>
+             <br/>
+             <a href="powersfamilybbq@gmail.com">Send Email</a>
+
+
+        </section>
     )
 }
 
