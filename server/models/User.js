3f7195629c48d2d16873985863f4c8f9a1efae60
Philip Powers <philip_powers607@hotmail.com>
2022-01-04 23:08:40 -0500

    where did this engine come from

--- server/models/User.js
+++ server/models/User.js
@@ -25,14 +25,14 @@ const UserSchema = new Schema(
     orders: [Order.schema]
   });
 
-  // UserSchema.pre('save', async function(next) {
-  //   if (this.isNew || this.isModified('password')) {
-  //     const saltRounds = 10;
-  //     this.password = await bcrypt.hash(this.password, saltRounds);
-  //   }
+  UserSchema.pre('save', async function(next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
   
-  //   next();
-  // });
+    next();
+  });
 
   UserSchema.methods.isCorrectPassword = async function(password) {
     return await bcrypt.compare(password, this.password);
